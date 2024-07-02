fcc82eb4361f7bc03e5e4e057b11a5e7e407b254
Andrii Romanovskyi <romanovskyi2137@gmail.com>
2024-07-02 12:06:22 +0300

    upd

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,16 @@
+import cl from "./Modal.module.css"
+
+
+export function Modal () {
+    
+
+    return (
+        <div className="">
+            <div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ModalState } from './context/ModalContext';
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <ModalState>
     <App/>
